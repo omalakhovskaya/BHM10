b7aa570e02320ddeb921241d2ef5ce2ac0ba674f
omalakhovskaya <oxana.malakhovskaya@gmail.com>
2018-03-17 12:23:55 +0300

    forward_backward_ready
    two function give the same result

--- forward.m
+++ forward.m
@@ -15,19 +15,11 @@ gamma = -A(5,4)*(D(3,3)*rho+D(3,3)*D(4,4)-A(3,4)*sigma_e_sq*D(4,4)-...
     D(3,3)*rho*A(3,4)*sigma_e_sq+rho*A(3,4)*sigma_e_sq^2-sigma_e_sq -...
     rho*sigma_e_sq-rho*A(3,4)*sigma_e_sq)^(-1);
 
-%tau = sigma_e_sq*gamma*(1+rho*A(3,4))*(rho+D(4,4)-rho*A(3,4)*sigma_e_sq)^(-1);
-%phi = gamma*sigma_e_sq/D(3,3)+rho*tau;
-
-
-
 D_tilde(1,1)= D(1,1);
 D_tilde(2,2)= D(2,2);
 D_tilde(3,3)= D(3,3);
-%D_tilde(3,4)= D(3,4) - (A(4,4)-1)*D(3,3)/A(3,4);
 D_tilde(3,4) = D(3,4) - rho*D(3,3);
-%D_tilde(4,3)= D(3,4) - rho*(D(3,4)-(A(4,4)-1)*D(3,3)/A(3,4))
 D_tilde(4,3) = D_tilde(3,4);
-%D_tilde(4,4)= D(4,4) +D(3,3)*(A(4,4)-1)^2/A(3,4)^2; 
 D_tilde(4,4) = D(4,4) - rho*D_tilde(3,4) ;
 D_tilde(3,5)= -A(3,4)*gamma*sigma_e_sq ;
 D_tilde(5,3)= -A(3,4)*gamma*sigma_e_sq ;
@@ -41,27 +33,11 @@ phi = -(D_tilde(3,5)+tau*D_tilde(3,4))/D_tilde(3,3);
 D_tilde(5,5) = D(5,5) - tau*(phi*D_tilde(3,4)+tau*D_tilde(4,4)+D_tilde(4,5))...
     -phi*D_tilde(3,5)-tau*D_tilde(4,5);
 
+G(4,3)= rho; 
+G(5,3) = phi;
+G(5,4) = tau;
 
-A_tilde(1,1) = 1;
-A_tilde(2,2)= 1;
-A_tilde(4,4) = 1;
-A_tilde(5,5)= 1;
-A_tilde(3,1)= 1;
-A_tilde(1,3) = A(1,3);
-A_tilde(2,3) = A(2,3);
-A_tilde(3,2) = A(3,2);
-A_tilde(3,4) = A(3,4);
-A_tilde(3,3) = A(3,3);
-A_tilde(4,1) = A(4,1) - rho; 
-A_tilde(4,2) = A(4,2) - rho*A(3,2); 
-A_tilde(4,3) = A(4,3) - rho*A(3,3); 
-A_tilde(5,4) = -gamma;
-A_tilde(5,1) =  A(5,1)- phi-tau*(A(4,1) - rho);
-A_tilde(5,2) =  A(5,2)- phi*A(3,2)-tau*(A(4,2) - rho*A(3,2));
-A_tilde(5,3) = A(5,3) - phi*A(3,3)-tau*(A(4,3) - rho*A(3,3));
-
-
-
+A_tilde = inv(G)*A;
 
 d_star(1) = D(1,1);
 d_star(2) = D(2,2);
@@ -73,9 +49,6 @@ d_star(5) = D(5,5) - gamma^2*sigma_e_sq;
 
 
 
-G(4,3)= rho; 
-G(5,3) = phi;
-G(5,4) = tau;
 
 
 
